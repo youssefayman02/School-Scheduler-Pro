dc1135464ee76a6de5f2816c9b3c6f4e6787fb49
Youssef Ayman <youssefabuzaid@hotmail.com>
2023-08-19 20:35:03 +0300

    Initial commit

---
+++ src/main/java/com/project/schoolschedulingsystem/SchoolSchedulingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.schoolschedulingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolSchedulingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolSchedulingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/schoolschedulingsystem/SchoolSchedulingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.schoolschedulingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolSchedulingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
